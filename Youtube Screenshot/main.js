55ef10a571c862c05f093aa4181b23e330a96ea1
Peter <62403514+PeterAyad@users.noreply.github.com>
2022-05-30 13:54:37 +0200

    Update main.js

--- Youtube Screenshot/main.js
+++ Youtube Screenshot/main.js
@@ -1,1 +1,35 @@
-document.onkeydown=(e=>{if(e.shiftKey&&"KeyA"==e.code){let e=document.getElementsByTagName("video")[0];if(e.paused||e.ended)return;e.pause();let t=document.createElement("canvas");t.width=e.videoWidth,t.height=e.videoHeight,t.getContext("2d").drawImage(e,0,0,e.videoWidth,e.videoHeight),e.play(),t.toBlob(e=>{let t=URL.createObjectURL(e);const o=document.createElement("a");o.href=t;var n=(new Date).toLocaleString("en-US").split(","),i=n[0].trim(),l=n[1].trim(),a=i.replaceAll("/","_"),d=l.substring(0,l.length-2).replaceAll(":","_");console.log("youtubeScreenshot_"+a+"_"+d),o.download="youtubeScreenshot_"+a+"_"+d,o.dispatchEvent(new MouseEvent("click",{bubbles:!0,cancelable:!0,view:window}))},"image/jpeg",1)}});
+document.onkeydown = (evt) => {
+    if (evt.shiftKey && evt.code == 'KeyA') {
+        let video = document.getElementsByTagName("video")[0];
+        if (video.paused || video.ended) {
+            return;
+        }
+        video.pause();
+        let c1 = document.createElement("canvas");
+        c1.width = video.videoWidth;
+        c1.height = video.videoHeight;
+        let context = c1.getContext("2d");
+        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+        video.play();
+        c1.toBlob((t) => {
+            let z = URL.createObjectURL(t);
+            const link = document.createElement('a');
+            link.href = z;
+            var d = new Date();
+            var x = d.toLocaleString('en-US').split(',');
+            var date = x[0].trim();
+            var time = x[1].trim();
+            var date_final = date.replaceAll('/', '_');
+            var time_final = time.substring(0, time.length - 2).replaceAll(":", "_");
+            console.log("youtubeScreenshot_" + date_final + "_" + time_final);
+            link.download = "youtubeScreenshot_" + date_final + "_" + time_final;
+            link.dispatchEvent(
+                new MouseEvent('click', {
+                    bubbles: true,
+                    cancelable: true,
+                    view: window
+                })
+            );
+        }, 'image/jpeg', 1);
+    }
+};
